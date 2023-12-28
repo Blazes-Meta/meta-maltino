62d120df13a71f2392bb0b0ea1c79b3df3426401
Annhilati <antbe.artis@web.de>
2023-12-28 15:57:12 +0100

    Update start.sh

--- .RASPBERRY/start.sh
+++ .RASPBERRY/start.sh
@@ -7,7 +7,7 @@ MAX_RAM=3584
 JAR_DATEI="SERVER DATEI"
 
 # Starte den Server in einem screen-Fenster
-screen -S minecraft-server -dm java -Xmx${MAX_RAM}M -Xms512M -jar ${JAR_DATEI} nogui 2> ${FEHLER_LOG_DATEI} > ${LOG_DATEI}
+screen -S minecraft-server -dm java -Xmx${MAX_RAM}M -Xms512M -jar ${JAR_DATEI} nogui
 
 # Gib eine Meldung aus, dass der Server gestartet wurde
 echo "Minecraft-Server gestartet. Verwende 'screen -r minecraft-server' zum Anzeigen der Konsole."
