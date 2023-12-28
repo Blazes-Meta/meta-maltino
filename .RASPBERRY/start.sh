8d56671d0389047c4d603b364283479b040598c7
Annhilati <antbe.artis@web.de>
2023-12-28 15:43:26 +0100

    Update start.sh

--- .RASPBERRY/start.sh
+++ .RASPBERRY/start.sh
@@ -4,10 +4,10 @@
 MAX_RAM=3584
 
 # Passe den Pfad zur Spigot/Bukkit JAR-Datei an
-JAR_DATEI="JAVA DATEI"
+JAR_DATEI="SERVER DATEI"
 
-# Passe die Dateinamen für das Log- und das Fehler-Log an
-LOG_DATEI="server.log"
-FEHLER_LOG_DATEI="fehler.log"
+# Starte den Server in einem screen-Fenster
+screen -S minecraft-server -dm java -Xmx${MAX_RAM}M -Xms512M -jar ${JAR_DATEI} nogui 2> ${FEHLER_LOG_DATEI} > ${LOG_DATEI}
 
-java -Xmx${MAX_RAM}M -Xms512M -jar ${JAR_DATEI} nogui 2> ${FEHLER_LOG_DATEI} > ${LOG_DATEI} &
+# Gib eine Meldung aus, dass der Server gestartet wurde
+echo "Minecraft-Server gestartet. Verwende 'screen -r minecraft-server' zum Anzeigen der Konsole."
