26522fb28241757170814f25bf4f101d7f7f0883
Annhilati <antbe.artis@web.de>
2024-01-02 12:23:12 +0100

    Update start.bat

--- .LOCALHOST/start.bat
+++ .LOCALHOST/start.bat
@@ -1,9 +1,9 @@
 @echo off
 set MAX_RAM=3584
-set JAR_DATEI="paper-1.20.1-196.jar"
+set JAR_DATEI=""
 
-:: Starte den Server in einem neuen CMD-Fenster
-start "Minecraft Server" java -Xmx%MAX_RAM%M -Xms512M -jar %JAR_DATEI% nogui
+:: Starte den Server im aktuellen CMD-Fenster
+java -Xmx%MAX_RAM%M -Xms512M -jar %JAR_DATEI% nogui
 
 :: Warte für eine Weile, damit der Server gestartet werden kann
 timeout /t 5 /nobreak >nul
