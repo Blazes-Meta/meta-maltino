2be5231fa5ac25ecf58fbdd80693bd2908368a2b
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-01-10 07:37:43 +0100

    Aktualisieren von start.sh

--- .LOCALHOST/start.sh
+++ .LOCALHOST/start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Setze die maximale RAM-Auslastung für den Server in Megabyte
 MAX_RAM=3584
-
-# Passe den Pfad zur Spigot/Bukkit JAR-Datei an
 JAR_DATEI="SERVER DATEI"
 
-# Starte den Server in einem screen-Fenster
 screen -S minecraft-server -dm java -Xmx${MAX_RAM}M -Xms512M -jar ${JAR_DATEI} nogui
 screen -r minecraft-server
