fc4848118164fef5c527aa2e901e2a1db0ba0d44
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-01-10 07:38:30 +0100

    Aktualisieren von start.bat

--- .LOCALHOST/start.bat
+++ .LOCALHOST/start.bat
@@ -2,10 +2,7 @@
 set MAX_RAM=3584
 set JAR_DATEI=""
 
-:: Starte den Server im aktuellen CMD-Fenster
 java -Xmx%MAX_RAM%M -Xms512M -jar %JAR_DATEI% nogui
-
-:: Warte für eine Weile, damit der Server gestartet werden kann
 timeout /t 5 /nobreak >nul
 
 echo Minecraft-Server gestartet. Verwende 'tasklist' und 'taskkill /F /PID <PID>' zum Beenden des Servers.
