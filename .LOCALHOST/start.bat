afec28c0f1184cb22e55dd120e68f884be2b0aa7
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-02-15 16:47:45 +0100

    Update start.bat

--- .LOCALHOST/start.bat
+++ .LOCALHOST/start.bat
@@ -1,9 +1,6 @@
 @echo off
+
 set MAX_RAM=3584
-set JAR_DATEI=""
+set JAR_DATEI="SERVER DATEI"
 
-java -Xmx%MAX_RAM%M -Xms512M -jar %JAR_DATEI% nogui
-timeout /t 5 /nobreak >nul
-
-echo Minecraft-Server gestartet. Verwende 'tasklist' und 'taskkill /F /PID <PID>' zum Beenden des Servers.
-PAUSE
+start "" java -Xmx%MAX_RAM%M -Xms512M -jar %JAR_DATEI% nogui
